9617f892acb37c16e2f750d240f06e828ce25026
Anton <ash2003@ukr.net>
2022-06-07 11:47:27 +0300

    Added pages for deleting new jobs and new applications by admin.

--- admin/home.py
+++ admin/home.py
@@ -6,6 +6,8 @@ from forms import NewJobForm
 from jobs import *
 from utils import *
 
+from freelancer.home import get_job_template
+
 admin = Blueprint(
     'admin', __name__,
     url_prefix='/admin',
@@ -56,3 +58,84 @@ def load_logged_in_user():
         g.user = {'role': 'admin'}
         g.user = get_admin(user_id)
         g.cursor = get_db_cursor()
+
+
+def get_new_applications_data():
+    g.cursor.execute(
+        'select * from get_all_new_applications();',
+    )
+    new_apps = g.cursor.fetchall()
+    for app in new_apps:
+        app['job_price'] = psql_money_to_dec(app['job_price'])
+        app['app_price'] = psql_money_to_dec(app['app_price'])
+
+    return new_apps
+
+def get_new_application_data(app_id):
+    g.cursor.execute(
+        'select * from get_new_application(app_id_p := %s);',
+        (app_id,)
+    )
+    app = g.cursor.fetchone()
+    if app:
+        app['job_price'] = psql_money_to_dec(app['job_price'])
+        app['app_price'] = psql_money_to_dec(app['app_price'])
+
+    return app
+
+
+@admin.route('/applications')
+def get_applications():
+    if g.user:
+        apps_data = get_new_applications_data()
+        apps_template = render_template('admin/applications_template.html', applications=apps_data)
+        return render_template('admin/new_applications.html', applications_template=apps_template)
+    return redirect(url_for('auth.login'))
+
+
+@admin.route('/explore_application/<int:app_id>', methods=['GET', 'POST'])
+def explore_application(app_id):
+    if g.user:
+        app_data = get_new_application_data(app_id)
+        if request.method == 'POST':
+            if request.form['submit'] == 'Delete application':
+                try:
+                    g.cursor.execute(
+                        """
+                        select * from remove_job_application_by_freelancer(job_id_p := %s, fr_id_p := %s);
+                        """,
+                        (app_data['job_id'], app_data['fr_id'])
+                    )
+                    g.db_conn.commit()
+                except Exception as e:
+                    flash(crop_psql_error(str(e)), 'danger')
+                else:
+                    flash(f"Application {app_data['app_description'][:20]}... was deleted!", 'success')
+                    return redirect(url_for('admin.get_applications'))
+
+        return render_template('admin/job_application.html', application=app_data)
+
+    return redirect(url_for('auth.login'))
+
+
+@admin.route('/explore_job/<int:job_id>', methods=['GET', 'POST'])
+def explore_job(job_id):
+    if g.user:
+        if request.method == 'POST':
+            if request.form['submit'] == 'Delete job':
+                try:
+                    g.cursor.execute(
+                        'select * from delete_job(job_id_p := %s);',
+                        (job_id,)
+                    )
+                    g.db_conn.commit()
+                except Exception as e:
+                    flash(crop_psql_error(str(e)), 'danger')
+                else:
+                    flash(f"Job was deleted!", 'success')
+                    return redirect(url_for('admin.index'))
+
+        job_template = get_job_template(job_id)
+        return render_template('admin/explore_job.html', job_template=job_template)
+
+    return redirect(url_for('auth.login'))
--- db_scripts/customer/customer_functions.sql
+++ db_scripts/customer/customer_functions.sql
@@ -34,7 +34,8 @@ drop function if exists accept_application_for_job(app_id_p integer, job_id_p in
 DROP FUNCTION IF EXISTS GET_JOB_PERFORMER(cust_id_p integer, job_id_p integer);
 
 
-
+DROP FUNCTION IF EXISTS GET_NEW_APPLICATION(app_id_p integer);
+DROP FUNCTION IF EXISTS GET_ALL_NEW_APPLICATIONS();
 DROP FUNCTION IF EXISTS GET_ACTIVE_CUSTOMER_APPLICATIONS(cust_id integer);
 DROP FUNCTION IF EXISTS GET_ALL_CUSTOMER_APPLICATIONS(cust_id_p integer);
 
@@ -170,8 +171,44 @@ CREATE TYPE CUSTOMER_APPLICATION
 AS (app_id integer, app_description varchar(450), app_time timestamp, app_price money,
     job_id integer, job_status project_status, job_is_blocked boolean,
     job_header varchar(250), job_description varchar(650), job_price money,
-    cust_f_name name_domain, cust_l_name name_domain, cust_id integer, cust_blocked boolean,
+    cust_f_name name_domain, cust_l_name name_domain, cust_id integer, cust_email email_domain, cust_blocked boolean,
     fr_f_name name_domain, fr_l_name name_domain, fr_id integer, fr_email email_domain, fr_blocked boolean);
+
+
+CREATE OR REPLACE FUNCTION GET_ALL_NEW_APPLICATIONS()
+RETURNS SETOF CUSTOMER_APPLICATION
+AS
+$$
+    begin
+        return query
+        select
+            app.id, app.description, app.date_time, app.price,
+            j.id, j.status, j.is_blocked, j.header_, j.description, j.price,
+            c.first_name, c.last_name, c.id, u2.email, c.is_blocked,
+            f.first_name, f.last_name, f.id, u.email, f.is_blocked
+        from application as app
+            inner join new_job as j on app.job_id = j.id
+            inner join customer as c on j.customer_id = c.id
+            inner join freelancer as f on app.freelancer_id = f.id
+            inner join users as u on f.user_id = u.id
+            inner join users as u2 on c.user_id = u2.id
+        where app.status = 'new'
+        order by app.date_time desc;
+    end;
+$$ language plpgsql;
+
+
+CREATE OR REPLACE FUNCTION GET_NEW_APPLICATION(app_id_p integer)
+RETURNS SETOF CUSTOMER_APPLICATION
+AS
+$$
+    begin
+        return query
+        select * from get_all_new_applications()
+        where app_id = app_id_p;
+
+    end;
+$$ language plpgsql;
 
 
 CREATE OR REPLACE FUNCTION GET_ALL_CUSTOMER_APPLICATIONS(cust_id_p integer)
@@ -182,13 +219,14 @@ AS $$
         select
             app.id, app.description, app.date_time, app.price,
             j.id, j.status, j.is_blocked, j.header_, j.description, j.price,
-            c.first_name, c.last_name, c.id, c.is_blocked,
+            c.first_name, c.last_name, c.id, u2.email, c.is_blocked,
             f.first_name, f.last_name, f.id, u.email, f.is_blocked
         from application as app
             inner join new_job as j on app.job_id = j.id
             inner join customer as c on j.customer_id = c.id
             inner join freelancer as f on app.freelancer_id = f.id
             inner join users as u on f.user_id = u.id
+            inner join users as u2 on c.user_id = u2.id
         where c.id = cust_id_p
         order by app.date_time desc;
     END;
